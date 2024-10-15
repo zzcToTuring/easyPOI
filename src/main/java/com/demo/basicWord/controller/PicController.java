99b37e74c7a82ac69012862873e91135852cee78
zzc_meg <zhangzicheng@megmeet.com>
2024-10-15 17:07:02 +0800

    导入vue，实现照片预览，调整文件顺序

--- src/main/java/com/demo/basicWord/controller/PicController.java
+++ src/main/java/com/demo/basicWord/controller/PicController.java
@@ -1,45 +1,45 @@
 package com.demo.basicWord.controller;
 
-import cn.afterturn.easypoi.entity.ImageEntity;
 import com.demo.basicWord.service.PicService;
+import com.demo.basicWord.util.ImageUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.logging.Handler;
 
-@RestController
+
+@Controller
 @RequestMapping("/pic")
 public class PicController {
     @Autowired
     private PicService picService;
 
-    @GetMapping(value = "down",name = "下载")
+    @Autowired
+    private ImageUtil imageUtil;
+
+    @GetMapping(value = "down1",name = "下载")
     public void down(HttpServletResponse response) throws Exception {
         picService.down(response);
     }
 
-    @GetMapping(value = "view",name="预览")
+    @GetMapping(value = "viewPic",name="预览")
+    @ResponseBody
+    @CrossOrigin()
     public ResponseEntity<FileSystemResource> getImage(HttpServletResponse response) throws Exception {
-        String imagePath = "src/main/resources/templates/pic.png"; // 图片的相对路径
-        FileSystemResource fileSystemResource = new FileSystemResource(Paths.get(imagePath));
-
-        if (!fileSystemResource.exists()) {
-            return ResponseEntity.notFound().build();
-        }
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.IMAGE_JPEG); // 设置图片类型
-        return ResponseEntity.ok().headers(headers).body(fileSystemResource);
+        return imageUtil.getImage("src/main/resources/templates/pic.png");
     }
 
+    @GetMapping(value = "view",name="预览")
+    public String getH()  {
+        return "../pic/pic.html";
+    }
 
 }
--- src/main/java/com/demo/basicWord/util/ImageUtil.java
+++ src/main/java/com/demo/basicWord/util/ImageUtil.java
@@ -1,10 +1,20 @@
 package com.demo.basicWord.util;
 
 import cn.afterturn.easypoi.entity.ImageEntity;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.nio.file.Paths;
 
 @Service
 public  class  ImageUtil {
+
+    //easyPOI返回需要的类型
      public ImageEntity imgFormatting(String imgPath, int width, int height) {
         //设置图片
         ImageEntity image = new ImageEntity(imgPath, width, height);
@@ -12,4 +22,16 @@ public  class  ImageUtil {
         image.setType(ImageEntity.URL);
         return image;
     }
+
+    @GetMapping(value = "view",name="预览")
+    public ResponseEntity<FileSystemResource> getImage(String imagePath) throws Exception {
+        FileSystemResource fileSystemResource = new FileSystemResource(Paths.get(imagePath));
+        if (!fileSystemResource.exists()) {
+            return ResponseEntity.notFound().build();
+        }
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_JPEG); // 设置图片类型
+        return ResponseEntity.ok().headers(headers).body(fileSystemResource);
+    }
+
 }
